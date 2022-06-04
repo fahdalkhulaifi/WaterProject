c0f8ae1996dc7cc3d0d88629f51e0e38d349be31
AbdulmagedALKHULAIFI <43535031+AbdulmagedALKHULAIFI@users.noreply.github.com>
2022-06-04 15:13:34 +0200

    Fixed bugs

---
+++ ViewModels/MakeRegistrationViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterNetworkProject.ViewModels
+{
+    public class MakeRegistrationViewModel : ViewModelBase
+    {
+        private int _userId;
+        public int UserId
+        {
+            get
+            {
+                return _userId;
+            }
+            set
+            {
+                _userId = value;
+                OnPropertyChanged(nameof(UserId));
+            }
+        }
+
+        private int _counterLecture;
+        public int CounterLecture
+        {
+            get
+            {
+                return _counterLecture;
+            }
+            set
+            {
+                _counterLecture = value;
+                OnPropertyChanged(nameof(CounterLecture));
+            }
+        }
+
+        private DateTime _registrationDate = DateTime.UtcNow;
+        public DateTime RegistrationDate
+        {
+            get
+            {
+                return _registrationDate;
+            }
+            set
+            {
+                _registrationDate = value;
+                OnPropertyChanged(nameof(RegistrationDate));
+            }
+        }
+    }
+}
